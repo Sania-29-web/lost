215a863bc81a297ab59b1dcd289e270e53ff6fde
Saniya Shaikh <saniashaikh54256@gmail.com>
2025-07-16 09:38:10 +0530

    Add files via upload

---
+++ perl.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+print"hello world\n";
+print"hellooooo\n";
+
+$a=7;
+print"$a\n";
+
+$result="this is number";
+print"$result\n";
+
+#uppercase 1
+$str="\Uwelcome to new world";
+print"$str\n";
+
+#uppercase whole line
+$str="\uwelcome to new world";
+print"$str\n";
+
+#space ko count karega or space pr slash ayega
+$str="\QWELCOME TO NEW WORLD";
+print"$str\n";
+
+$age=25;#an integer assign
+$name="John mohn";#a string
+$salary="145,50";#a floating point
+
+print"Age=$age\n";
+print"Name=$name\n";
+print"Salary=$salary\n";
+
+
+@age=(25,19,39);
+@names=("hima","rema","sema");
+print"\$age[0]=$age[0]\n";
+print"\$age[1]=$age[1]\n";
+print"\$age[2]=$age[2]\n";
+print"\$name[0]=$names[0]\n";
+print"\$name[2]=$names[2]\n";
+
+@array=(1,2,"Hello");
+@array=qw/Thisis an array/;
+
+@days=qw/Monday
+Tuesday
+...
+Sunday/;
+print"\$name[1]=$names[1]\n";
